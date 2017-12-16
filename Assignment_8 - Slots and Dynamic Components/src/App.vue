8270bd095261942a893adac1885b4d2b044f9e3a
Alex Schin <schinalex@gmail.com>
2017-12-16 08:39:51 +0200

    reorganised things

--- Assignment_8 - Slots and Dynamic Components/src/App.vue
+++ Assignment_8 - Slots and Dynamic Components/src/App.vue
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="selectedComponent">
+                  <p>Hello World!</p>
+                </component>
             </div>
         </div>
     </div>
@@ -25,7 +25,10 @@
             appBlue: Blue,
             appGreen: Green,
             appRed: Red
-        }
+        },
+        data: () => ({
+          selectedComponent: 'appBlue'
+        })
     }
 </script>
 
